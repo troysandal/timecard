5042a39a6c1a6993bbf13b6bdce465dd5ed5d336
Troy Sandal <troysandal@gmail.com>
2025-02-21 12:00:31 -0800

    remove extraneous assets, fix pwa warning

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <svelte:head>
-  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="mobile-web-app-capable" content="yes">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   <link rel="apple-touch-icon" href={logo} />
   <meta name="apple-mobile-web-app-title" content="Enduro Time Card" />
